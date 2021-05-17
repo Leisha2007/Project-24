d6c407052191a32ce3e3409097756b640b140df5
Leisha2007 <74778826+Leisha2007@users.noreply.github.com>
2021-05-17 17:07:43 +0530

    Update Iron.js

--- Iron.js
+++ Iron.js
@@ -5,10 +5,19 @@ class Iron{
 		 friction:3,
 		 density:30
      }
-		this.x=x;
-		this.y=y;
+		this.width=20;
 		this.body=Bodies.rectangle(x,y,this.width,this.width);
-	    World.add(world, this.body)
-        display()
+	    World.add(world, this.body);
     }
+	display(){
+		var ironposition=this.body.position;
+		push();
+		translate(ironposition.x,ironposition.y);
+		strokeWeight(3);
+        stroke('white');
+        fill('black');
+		rectMode(CENTER);
+        rect(0, 0, this.width, this.height);
+		pop();
+	}
 }
