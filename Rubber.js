c44c130c87d757c99df9d21733be946fd9e7337d
Leisha2007 <74778826+Leisha2007@users.noreply.github.com>
2021-05-17 22:29:34 +0530

    Update Rubber.js

--- Rubber.js
+++ Rubber.js
@@ -9,7 +9,7 @@ class Rubber{
 		this.x=x;
 		this.y=y;
 		this.r=r;
-		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options);
+		this.body=Bodies.circle(this.x, this.y, this.r, options);
 		World.add(world, this.body);
 	}
 	display()
