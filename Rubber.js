8062edf372fedd4222f07b924837b942eecd737c
Leisha2007 <74778826+Leisha2007@users.noreply.github.com>
2021-05-17 17:09:21 +0530

    Update Rubber.js

--- Rubber.js
+++ Rubber.js
@@ -8,22 +8,20 @@ class Rubber{
 	 }
 		this.x=x;
 		this.y=y;
-		this.r=r
-		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		this.r=r;
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options);
 		World.add(world, this.body);
-
 	}
 	display()
 	{
-			var rubberpos=this.body.position;		
-			push()
-			translate(rubberpos.x, rubberpos.y);
-			rectMode(CENTER)
+			var rubberposition=this.body.position;		
+			push();
+			translate(rubberposition.x, rubberposition.y);
+			rectMode(CENTER);
 			strokeWeight(4);
 			stroke("black");
 			fill("darkblue");
 			ellipse(0,0,this.r,this.r);
-			pop()
+			pop();
 	}
-
 }
