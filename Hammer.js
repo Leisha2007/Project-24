f2d63347e1fe6f01224563b8bffa13f88f488ef7
Leisha2007 <74778826+Leisha2007@users.noreply.github.com>
2021-05-17 17:06:17 +0530

    Update Hammer.js

--- Hammer.js
+++ Hammer.js
@@ -4,12 +4,12 @@ class Hammer {
       'density':4,
       'friction': 1.0,
       'restitution':0.5
-    };
+    }
     this.body = Bodies.rectangle(x, y, 50, 50, options);
     this.width = 150;
     this.height = 40;
     World.add(world, this.body);
-  };
+  }
   display(){
     var pos = this.body.position;
     pos.x = mouseX;
@@ -20,10 +20,10 @@ class Hammer {
     translate(pos.x, pos.y);
     rotate(angle);
     strokeWeight(3);
-    stroke('white')
-    fill('orange')
-    rectMode(CENTER)
+    stroke('white');
+    fill('orange');
+    rectMode(CENTER);
     rect(0, 0, this.width, this.height);
     pop();
-  };
-};
+  }
+}
