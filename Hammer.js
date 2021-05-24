9bf061d3abfb18eadd2f79ffa4d0a234300d5b99
Leisha2007 <74778826+Leisha2007@users.noreply.github.com>
2021-05-24 20:28:10 +0530

    Update Hammer.js

--- Hammer.js
+++ Hammer.js
@@ -5,7 +5,7 @@ class Hammer {
       'friction': 1.0,
       'restitution':0.5
     }
-    this.body = Bodies.rectangle(x, y, 50, 50, options);
+    this.body = Bodies.rectangle(x, y, 150, 40, options);
     this.width = 150;
     this.height = 40;
     World.add(world, this.body);
